7e625b685187d0c77af34d71f3a1da38f817b9cf
Paulina Zimny <paulina.zimny@gmail.com>
2021-10-10 10:56:17 +0200

    Display issues grouped by date, fix styles

--- components/DailyIssues/DailyIssues.js
+++ components/DailyIssues/DailyIssues.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { issuesMock } from "../../utils/mocks/issues-mock";
 import Issue from "../Issue/Issue";
 import "./DailyIssues.scss";
 
-const DailyIssues = () => {
+const DailyIssues = ({ issues }) => {
   return (
     <div className="daily-issues">
-      <h6 className="daily-issues__date">19-07-2016</h6>
-      {issuesMock.map((i, index) => (
+      <h6 className="daily-issues__date">{issues[0].date}</h6>
+      {issues.map((i, index) => (
         <Issue key={index} open={i.open} name={i.name} id={i.id} />
       ))}
     </div>
--- components/IssuesList/IssuesList.js
+++ components/IssuesList/IssuesList.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import "./IssuesList.scss";
+import DailyIssues from "../DailyIssues/DailyIssues";
+import { issuesMock } from "../../utils/mocks/issues-mock";
+import _ from "lodash";
 
 const IssuesList = () => {
-  return <div>issues</div>;
+  const [groupedIssues, setGroupedIssues] = useState();
+
+  useEffect(() => {
+    const grouped = _.mapValues(_.groupBy(issuesMock, "date"), (list) =>
+      list.map((i) => i)
+    );
+    setGroupedIssues(grouped);
+  }, []);
+
+  return (
+    <div className="issues-list">
+      {groupedIssues &&
+        Object.keys(groupedIssues).map((k, i) => (
+          <DailyIssues key={i} issues={groupedIssues[k]} />
+        ))}
+    </div>
+  );
 };
 
 export default IssuesList;
